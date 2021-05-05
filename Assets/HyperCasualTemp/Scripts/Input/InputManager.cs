46e92d6e25b0b7b782371751ef70713bc00a238a
UGURINAL <ugurinal01@hotmail.com>
2021-05-05 21:26:08 +0300

    Hyper Casual Temp
    +added: preprocessors for input manager

--- Assets/HyperCasualTemp/Scripts/Input/InputManager.cs
+++ Assets/HyperCasualTemp/Scripts/Input/InputManager.cs
@@ -24,33 +24,44 @@ namespace HyperCasualTemp.PlayerInput
 
         private void Update()
         {
-            if (Input.touchCount <= 0) return;
+#if UNITY_EDITOR
+            Debug.Log("UNITY EDITOR!");
+            _touchDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized *
+                              200f;
 
-            Touch touch = Input.GetTouch(0);
+#endif
 
-            switch (touch.phase)
+#if UNITY_ANDROID && !UNITY_EDITOR
+            Debug.Log("ANDROID!");
+            if (Input.touchCount > 0)
             {
-                case TouchPhase.Began:
-                    _startTouchPos = touch.position;
-                    _currentTouchPos = touch.position;
-                    break;
-                case TouchPhase.Moved:
-                    _currentTouchPos = touch.position;
-                    ComputeTouchDirection(_currentTouchPos, _startTouchPos);
-                    break;
-                case TouchPhase.Stationary:
-                    Debug.Log("STATIONARY!");
-                    break;
-                case TouchPhase.Ended:
-                    _touchDirection = Vector3.zero;
-                    break;
-                case TouchPhase.Canceled:
-                    _touchDirection = Vector3.zero;
-                    break;
-                default:
-                    Debug.Log("DEFAULT INPUT!");
-                    break;
+                Touch touch = Input.GetTouch(0);
+
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        _startTouchPos = touch.position;
+                        _currentTouchPos = touch.position;
+                        break;
+                    case TouchPhase.Moved:
+                        _currentTouchPos = touch.position;
+                        ComputeTouchDirection(_currentTouchPos, _startTouchPos);
+                        break;
+                    case TouchPhase.Stationary:
+                        Debug.Log("STATIONARY!");
+                        break;
+                    case TouchPhase.Ended:
+                        _touchDirection = Vector3.zero;
+                        break;
+                    case TouchPhase.Canceled:
+                        _touchDirection = Vector3.zero;
+                        break;
+                    default:
+                        Debug.Log("DEFAULT INPUT!");
+                        break;
+                }
             }
+#endif
         }
 
         private void FixedUpdate()
